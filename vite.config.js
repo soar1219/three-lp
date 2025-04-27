9e51f4ab1df1ba2f84b7b5de7f8f4f6181697fa1
soar1219 <tsubasa1219.info@gmail.com>
2025-04-28 00:47:11 +0900

    gitに反映できるように修正

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES
+    ? "/reactailwind/" // ←ここはGitHub Pages用に絶対パス "/"付きで！
+    : "./",
+  build: {
+    outDir: "docs", // ★ビルド成果物を docs/ に出す
+    assetsDir: "assets", // ★assetsはassets/にまとめる
+  },
   plugins: [react()],
 })
