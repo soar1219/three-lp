45eda75f888052371b24cbd52e65a3d2ce26fca5
soar1219 <tsubasa1219.info@gmail.com>
2025-04-28 20:48:41 +0900

    20250428

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <canvas class="webgl"></canvas>
+      <main className="section">
+        <div className="content">
+          <h1>未到達の次元へ。</h1>
+          <p>ありふれたエンジニアで終わらせない。<br />人々を感動させる技術を身につける。<br />あなたが求めるスキルが、今ここに。</p>
+          <button>私の作品へ</button>
+        </div>
+      </main>
     </>
   )
 }
